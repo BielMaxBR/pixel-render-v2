89df9cee68ae74449c9e7096bc84f5c1240e032b
BielMaxBR <gbielmaximo@gmail.com>
2025-11-22 17:48:03 -0300

    builder

--- build.bat
+++ build.bat
@@ -1,1 +1,9 @@
-g++ -o build/test.exe src/*.cpp -I include -L lib -lraylib -lgdi32 -lwinmm -limm32 && build/test.exe
+@echo off
+if not exist build cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S . -B build
+
+echo Compilando...
+cmake --build build
+
+echo Rodando o jogo...
+build\out\game.exe
+pause
