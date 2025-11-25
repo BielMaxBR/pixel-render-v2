4addfb0b4a3e0269139eab3383260098922d2783
BielMaxBR <gbielmaximo@gmail.com>
2025-11-25 14:19:40 -0300

    tudo R3D

--- build.bat
+++ build.bat
@@ -2,8 +2,4 @@
 if not exist build cmake -G "MinGW Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S . -B build
 
 echo Compilando...
-cmake --build build
-
-echo Rodando o jogo...
-build\out\game.exe
-pause
+cmake --build build && build\out\game.exe
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "imgui.h"
-#include "raylib.h"
-#include "rlImGui.h"
 #include "r3d.h"
+#include "raylib.h"
+#include "raymath.h"
+#include "rlImGui.h"
 
 class DocumentWindow {
 public:
@@ -21,42 +22,62 @@ public:
 
 class Scene : public DocumentWindow {
 public:
+
   Camera3D Camera = {0};
+
+  R3D_Mesh meshes[10];
+  R3D_Material material;
+  R3D_Light light;
   float x = 0;
   void Setup() override {
     ViewTexture = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
 
-    Camera.fovy = 45;
-    Camera.up.y = 1;
-    Camera.position.y = 3;
-    Camera.position.z = -25;
+    Camera = {.position = {-3, 3, 3},
+              .target = {0, 0, 0},
+              .up = {0, 1, 0},
+              .fovy = 90.0f,
+              .projection = CAMERA_PERSPECTIVE};
+    // Create scene objects
 
-    ImGuiIO& io = ImGui::GetIO();
+    meshes[0] = R3D_GenMeshSphere(1.0f, 16, 32, true);
+    meshes[1] = R3D_GenMeshPlane(10.0f,10.0f,10,10,true);
+    material = R3D_GetDefaultMaterial();
+
+    // Setup lighting
+    light = R3D_CreateLight(R3D_LIGHT_DIR);
+    R3D_SetLightDirection(light, (Vector3){-1, -1, -1});
+    R3D_SetLightActive(light, true);
+    R3D_EnableShadow(light, 20);
+    R3D_SetBackgroundColor(BLANK);
+    R3D_SetAmbientColor(BLANK);
+
+    ImGuiIO &io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
   };
   void Update() override {
     if (!Open)
       return;
-    x+=0.1;
+    x += 0.1;
     if (IsWindowResized()) {
       UnloadRenderTexture(ViewTexture);
       ViewTexture = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
     }
 
-    BeginTextureMode(ViewTexture);
-    ClearBackground(LIGHTGRAY);
-    BeginMode3D(Camera);
-
-    DrawPlane(Vector3{ 0, 0, 0 }, Vector2{ 50, 50 }, BEIGE);
-    DrawCube(Vector3{0, x, 0}, 5, 5, 5, Color{255, 255, 200});
-
-    EndMode3D();
-
-    EndTextureMode();
+    R3D_SetRenderTarget(&ViewTexture);
+    R3D_Begin(Camera);
+    for (int i = 0; i < 2; i++) {
+    R3D_DrawMesh(&meshes[i], &material, MatrixIdentity());
+    }
+    R3D_End();
   };
-  void Shutdown() override { UnloadRenderTexture(ViewTexture); }
+  void Shutdown() override {
+    for (int i = 0; i < 2; i++) {
+    R3D_UnloadMesh(&meshes[i]);
+    }
+    UnloadRenderTexture(ViewTexture);
+  }
   void Show() override {
-    ImGui::SetNextWindowSizeConstraints(ImVec2(200,200),ImVec2(2000,2000));
+    ImGui::SetNextWindowSizeConstraints(ImVec2(200, 200), ImVec2(2000, 2000));
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
     if (ImGui::Begin("3D View", &Open, ImGuiWindowFlags_NoScrollbar)) {
       Focused = ImGui::IsWindowFocused(ImGuiFocusedFlags_ChildWindows);
@@ -68,38 +89,34 @@ public:
   }
 };
 
-void showDockSpace(){
-    static bool dockingEnabled = true;
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+void showDockSpace() {
+  static bool dockingEnabled = true;
+  ImGuiIO &io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
-    ImGuiWindowFlags window_flags = 
-        ImGuiWindowFlags_NoTitleBar |
-        ImGuiWindowFlags_NoCollapse |
-        ImGuiWindowFlags_NoResize |
-        ImGuiWindowFlags_NoMove |
-        ImGuiWindowFlags_NoBringToFrontOnFocus |
-        ImGuiWindowFlags_NoNavFocus |
-        ImGuiWindowFlags_NoBackground;
+  ImGuiWindowFlags window_flags =
+      ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+      ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+      ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus |
+      ImGuiWindowFlags_NoBackground;
 
-    ImGuiViewport* viewport = ImGui::GetMainViewport();
+  ImGuiViewport *viewport = ImGui::GetMainViewport();
 
-    ImGui::SetNextWindowPos(viewport->Pos);
-    ImGui::SetNextWindowSize(viewport->Size);
-    ImGui::SetNextWindowViewport(viewport->ID);
+  ImGui::SetNextWindowPos(viewport->Pos);
+  ImGui::SetNextWindowSize(viewport->Size);
+  ImGui::SetNextWindowViewport(viewport->ID);
 
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+  ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+  ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+  ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
 
-    ImGui::Begin("DockSpaceHost", nullptr, window_flags);
+  ImGui::Begin("DockSpaceHost", nullptr, window_flags);
 
-    ImGui::PopStyleVar(3);
-    ImGuiID dockspace_id = ImGui::GetID("MainDockSpace");
-    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f));
+  ImGui::PopStyleVar(3);
+  ImGuiID dockspace_id = ImGui::GetID("MainDockSpace");
+  ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f));
 
-    ImGui::End();
-
+  ImGui::End();
 }
 int main() {
   const int screenWidth = 1200;
@@ -122,12 +139,13 @@ int main() {
     BeginDrawing();
     ClearBackground(DARKGRAY);
     // start ImGui Conent
-    rlImGuiBegin(); 
+    rlImGuiBegin();
     // show ImGui Content
     showDockSpace();
     ImGui::ShowDemoWindow(&open);
 
-    if (cena.Open) cena.Show();
+    if (cena.Open)
+      cena.Show();
 
     // salvador de padrão
     // if (ImGui::Button("save", ImVec2(4,4))) {
@@ -135,6 +153,7 @@ int main() {
     // }
 
     // end ImGui Content
+
     rlImGuiEnd();
     EndDrawing();
   }
