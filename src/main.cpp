4a8c022fa5d0a4e08e39ac481295cd1e9c2bb7fa
BielMaxBR <gbielmaximo@gmail.com>
2025-11-26 16:44:36 -0300

    de volta ao básico

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "imgui.h"
-#include "r3d.h"
+// #include "r3d.h"
 #include "raylib.h"
-#include "raymath.h"
+// #include "raymath.h"
 #include "rlImGui.h"
 
 class DocumentWindow {
@@ -24,35 +24,40 @@ class Scene : public DocumentWindow {
 public:
 
   Camera3D Camera = {0};
+  Model model;
+  Material material;
+  // R3D_Light light;
 
-  R3D_Mesh meshes[10];
-  R3D_Material material;
-  R3D_Light light;
+  int sprite_width = 128;
+  int sprite_height = 128;
   float x = 0;
   void Setup() override {
-    ViewTexture = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
+    // ViewTexture = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
+    ViewTexture = LoadRenderTexture(sprite_width, sprite_height);
+    // R3D_UpdateResolution(sprite_width,sprite_height);
 
-    Camera = {.position = {-3, 3, 3},
+    Camera = {.position = {3, 3, -3},
               .target = {0, 0, 0},
               .up = {0, 1, 0},
-              .fovy = 90.0f,
+              .fovy = 75.0f,
               .projection = CAMERA_PERSPECTIVE};
     // Create scene objects
 
-    meshes[0] = R3D_GenMeshSphere(1.0f, 16, 32, true);
-    meshes[1] = R3D_GenMeshPlane(10.0f,10.0f,10,10,true);
-    material = R3D_GetDefaultMaterial();
+    // SetTextureFilter(Texture2D texture, int filter)
+
+    model = LoadModel("neco.glb");
+    // model = R3D_LoadModel("neco.fbx");
+    // meshes[0] = R3D_GenMeshSphere(1.0f, 16, 32, true);
+    // meshes[0] = R3D_GenMeshPlane(10.0f,10.0f,10,10,true);
+    // material = R3D_GetDefaultMaterial();
 
     // Setup lighting
-    light = R3D_CreateLight(R3D_LIGHT_DIR);
-    R3D_SetLightDirection(light, (Vector3){-1, -1, -1});
-    R3D_SetLightActive(light, true);
-    R3D_EnableShadow(light, 20);
-    R3D_SetBackgroundColor(BLANK);
-    R3D_SetAmbientColor(BLANK);
-
-    ImGuiIO &io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    // light = R3D_CreateLight(R3D_LIGHT_DIR);
+    // R3D_SetLightDirection(light, (Vector3){-1, 1, 1});
+    // R3D_SetLightActive(light, true);
+    // R3D_EnableShadow(light, 200);
+    // R3D_SetBackgroundColor(BLANK);
+    // R3D_SetAmbientColor(WHITE);
   };
   void Update() override {
     if (!Open)
@@ -60,29 +65,44 @@ public:
     x += 0.1;
     if (IsWindowResized()) {
       UnloadRenderTexture(ViewTexture);
-      ViewTexture = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
+      ViewTexture = LoadRenderTexture(sprite_width, sprite_height);
     }
 
-    R3D_SetRenderTarget(&ViewTexture);
-    R3D_Begin(Camera);
-    for (int i = 0; i < 2; i++) {
-    R3D_DrawMesh(&meshes[i], &material, MatrixIdentity());
-    }
-    R3D_End();
+    BeginTextureMode(ViewTexture);
+    ClearBackground(BLANK);
+    BeginMode3D(Camera);
+
+    // DrawPlane(Vector3{ 0, 0, 0 }, Vector2{ 50, 50 }, BEIGE);
+    DrawModel(model, Vector3{0,0,0}, 1, WHITE);
+    DrawGrid(10, 1.0f);
+    EndMode3D();
+    EndTextureMode();
+    // R3D_SetRenderTarget(&ViewTexture);
+    // R3D_Begin(Camera);
+    // // BlendMode(BLEND_ALPHA);
+    // // for (int i = 0; i < 1; i++) {
+    // // R3D_DrawMesh(&meshes[i], &material, MatrixIdentity());
+    // // }
+    // // R3D_DrawModel(&model, Vector3{0,0,0},1);
+    // R3D_End();
   };
   void Shutdown() override {
-    for (int i = 0; i < 2; i++) {
-    R3D_UnloadMesh(&meshes[i]);
-    }
+    UnloadModel(model);
     UnloadRenderTexture(ViewTexture);
   }
   void Show() override {
-    ImGui::SetNextWindowSizeConstraints(ImVec2(200, 200), ImVec2(2000, 2000));
+    ImGui::SetNextWindowSizeConstraints(ImVec2(sprite_width, sprite_height), ImVec2(2000, 2000));
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
     if (ImGui::Begin("3D View", &Open, ImGuiWindowFlags_NoScrollbar)) {
       Focused = ImGui::IsWindowFocused(ImGuiFocusedFlags_ChildWindows);
       // draw the view
       rlImGuiImageRenderTextureFit(&ViewTexture, true);
+
+      if (ImGui::Button("save", ImVec2(4,4))) {
+        Image img = LoadImageFromTexture(ViewTexture.texture);
+        ImageFlipVertical(&img);
+        ExportImage(img,"final.png");
+      }
     }
     ImGui::End();
     ImGui::PopStyleVar();
@@ -91,8 +111,6 @@ public:
 
 void showDockSpace() {
   static bool dockingEnabled = true;
-  ImGuiIO &io = ImGui::GetIO();
-  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
   ImGuiWindowFlags window_flags =
       ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
@@ -121,11 +139,15 @@ void showDockSpace() {
 int main() {
   const int screenWidth = 1200;
   const int screenHeight = 700;
-  SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT);
+  SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
   InitWindow(screenWidth, screenHeight, "Pixel Render");
-  R3D_Init(screenWidth, screenHeight, 0);
+  // R3D_Init(screenWidth, screenHeight, R3D_FLAG_ASPECT_KEEP);
+  // R3D_SetState(R3D_FLAG_ASPECT_KEEP);
   rlImGuiSetup(true);
   SetTargetFPS(60);
+
+  ImGuiIO &io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
   Scene cena;
   bool open = true;
@@ -146,7 +168,7 @@ int main() {
 
     if (cena.Open)
       cena.Show();
-
+    
     // salvador de padrão
     // if (ImGui::Button("save", ImVec2(4,4))) {
     //   ImGui::SaveIniSettingsToDisk("default.ini");
@@ -159,7 +181,7 @@ int main() {
   }
   cena.Shutdown();
   rlImGuiShutdown();
-  R3D_Close();
+  // R3D_Close();
   CloseWindow();
   return 0;
 }
