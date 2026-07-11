f91f2322141ced501b2f60ce98cb0b073f3a5cca
BielMaxBR <gbielmaximo@gmail.com>
2026-07-11 16:29:50 -0300

    voltei :D

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not exist build cmake -G "MinGW Makefiles" -DGRAPHICS_API_OPENGL_11 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S . -B build
-
-echo Compilando...
-cmake --build build && build\out\PixelRender.exe
---
+++ readme.md
@@ -0,0 +1,7 @@
+# instalar
+## linux
+ sudo pacman -S alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama
+
+# rodar
+ cmake --preset linux (só uma vez)
+ cmake --build --preset run-linux 
